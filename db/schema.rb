3923419cf6982c7a45104a9cb4c268c1406a0786
Setiadi <setiadi.seth@gmail.com>
2023-03-28 13:49:07 +0700

    Update homepage layout links

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20230328044154) do
+ActiveRecord::Schema.define(version: 20230328060601) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
